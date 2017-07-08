109c69e360c369199e4952d06f6f91565615cd87
Jordan Hudgens <jordan@devcamp.com>
2017-07-08 19:55:22 +0000

    Added portfolio items via the resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708182102) do
+ActiveRecord::Schema.define(version: 20170708193241) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.text     "body"
+    t.text     "main_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
